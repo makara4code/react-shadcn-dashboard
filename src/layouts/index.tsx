1e6217514ac166358a5dea3e6156eeaecce9e543
MAKARASAMA\Admin <nuolmakara@gmail.com>
2024-06-10 15:54:21 +0900

    feat: active side nav

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -33,12 +33,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export function Layout() {
+  const getNavLinkClass = (isActive: boolean) => {
+    return `flex items-center gap-3 px-3 py-2 transition-all rounded-lg ${
+      isActive
+        ? "bg-muted text-primary"
+        : "text-muted-foreground hover:text-primary"
+    }`;
+  };
+  
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex flex-col h-full max-h-screen gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link to="/" className="flex items-center gap-2 font-semibold">
+            <Link
+              to="/dashboard"
+              className="flex items-center gap-2 font-semibold">
               <Package2 className="w-6 h-6" />
               <span className="">Acme Inc</span>
             </Link>
@@ -47,32 +57,26 @@ export function Layout() {
               <span className="sr-only">Toggle notifications</span>
             </Button>
           </div>
-
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <NavLink
                 to="/dashboard"
-                className={({ isActive }) =>
-                  isActive
-                    ? "flex items-center gap-3 px-3 py-2 transition-all rounded-lg bg-muted text-primary hover:text-primary"
-                    : "flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-                }>
+                className={({ isActive }) => getNavLinkClass(isActive)}>
                 <Home className="w-4 h-4" />
                 Dashboard
               </NavLink>
+
               <NavLink
                 to="/order"
-                className={({ isActive }) =>
-                  isActive
-                    ? "flex items-center gap-3 px-3 py-2 transition-all rounded-lg bg-muted text-primary hover:text-primary"
-                    : "flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-                }>
-                <ShoppingCart className="w-5 h-5" />
-                Order
+                className={({ isActive }) => getNavLinkClass(isActive)}>
+                <ShoppingCart className="w-4 h-4" />
+                Orders
+                <Badge className="flex items-center justify-center w-6 h-6 ml-auto rounded-full shrink-0">
+                  6
+                </Badge>
               </NavLink>
             </nav>
           </div>
-
           <div className="p-4 mt-auto">
             <Card x-chunk="dashboard-02-chunk-0">
               <CardHeader className="p-2 pt-0 md:p-4">
@@ -91,9 +95,8 @@ export function Layout() {
           </div>
         </div>
       </div>
-
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 ">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
@@ -104,7 +107,7 @@ export function Layout() {
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col bg-green-200">
+            <SheetContent side="left" className="flex flex-col">
               <nav className="grid gap-2 text-lg font-medium">
                 <Link
                   to="#"
@@ -171,7 +174,7 @@ export function Layout() {
                 <Input
                   type="search"
                   placeholder="Search products..."
-                  className="w-full pl-8 shadow-none Layoutearance-none bg-background md:w-2/3 lg:w-1/3"
+                  className="w-full pl-8 shadow-none appearance-none bg-background md:w-2/3 lg:w-1/3"
                 />
               </div>
             </form>
